01699638b2c0713e47a318766d7f787079111f50
Matheus Mombach <matheus.mombach@sap.com>
2018-07-24 23:17:13 -0300

    Implement AbstractCommand class.

--- source/command/Command.js
+++ source/command/Command.js
@@ -5,4 +5,17 @@ var CommandType;
     CommandType[CommandType["ToDo"] = 0] = "ToDo";
     CommandType[CommandType["History"] = 1] = "History";
 })(CommandType = exports.CommandType || (exports.CommandType = {}));
+var AbstractCommand = /** @class */ (function () {
+    function AbstractCommand(commandArguments) {
+        this.arguments = commandArguments;
+    }
+    AbstractCommand.prototype.setArguments = function (commandArguments) {
+        this.arguments = commandArguments;
+    };
+    AbstractCommand.prototype.execute = function () {
+        console.log("Default command.");
+    };
+    return AbstractCommand;
+}());
+exports.AbstractCommand = AbstractCommand;
 //# sourceMappingURL=Command.js.map
--- source/command/Command.ts
+++ source/command/Command.ts
@@ -7,3 +7,19 @@ export enum CommandType {
   ToDo = 0,
   History = 1
 }
+
+export abstract class AbstractCommand implements Command {
+  protected arguments: string[];
+
+  constructor(commandArguments: string[]) {
+    this.arguments = commandArguments;
+  }
+
+  public setArguments(commandArguments: string[]) {
+    this.arguments = commandArguments;
+  }
+
+  public execute(): void {
+    console.log("Default command.");
+  }
+}
