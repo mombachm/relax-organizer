f078f6977a2e07f82890b817e7b19992b3980ae6
Matheus Mombach <matheus.mombach@sap.com>
2018-08-19 22:33:47 -0300

    Update Task item to have description.

--- source/logitem/todo/Task.ts
+++ source/logitem/todo/Task.ts
@@ -1,8 +1,14 @@
 import { LogItem } from "../LogItem";
 
 export class Task extends LogItem{
+  private description: string | undefined;
 
-  constructor() {
+  constructor(description?: string) {
     super();
+    this.description = description;
+  }
+
+  public setDescription(description: string): void {
+    this.description = description;
   }
 }
